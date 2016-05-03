8ebd746548a1484096387068925e02a9390b0fa6
joecala@umich.edu <joecala@umich.edu>
2016-05-03 15:45:16 -0400

    Added comment

--- read_gm_data.R
+++ read_gm_data.R
@@ -2,6 +2,8 @@ gdp <- read.csv("data/data-1952.csv")
 new <- read.csv("data/data-1957.csv")
 gdp <- rbind(gdp, new) #pipes these together#
 
+#Calculating gdp by joecala
+
 gdp['gdp'] <- gdp$gdpPercap * gdp$pop
 
 keep <- c('country', 'year', 'lifeExp', 'gdp')
